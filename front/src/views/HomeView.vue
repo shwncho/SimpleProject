76a09b300016af8bb2c18b9686aa106fb75ad3fc
simple <shwncho4@gmail.com>
2022-09-18 20:30:41 +0900

    feat: front, posts list

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+  import axios from "axios";
+  import {ref} from "vue";
+
+  const posts = ref([]);
+
+  axios.get("http://localhost:8080/posts?page=1&&size=10")
+  .then(response =>{
+    response.data.forEach((r:any)=>{
+      posts.value.push(r);
+    })
+  })
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <div>
+        {{ post.title}}
+      </div>
+      <div>
+        {{ post.content}}
+      </div>
+    </li>
+  </ul>
 </template>
+
+<style scoped>
+  li{
+    margin-bottom: 1rem;
+  }
+  li:last-child{
+    margin-bottom: 0;
+  }
+</style>
--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
   import {ref} from "vue";
   import axios from 'axios';
+  import {useRouter} from "vue-router";
 
 
   const title =ref("")
   const content=ref("")
+  const router = useRouter();
 
   const write=function (){
     axios.post("http://localhost:8080/posts",{
       title: title.value,
       content: content.value
+    })
+    .then(()=>{
+      /**
+       * push 대신 replace 사용
+       * => 글 작성하고 홈 화면에서 뒤로가기 눌렀을 때 다시 글 작성화면으로 라우팅 되는거 방지할 수 있다.
+       */
+      router.replace({name: "home"});
     });
   }
 </script>
