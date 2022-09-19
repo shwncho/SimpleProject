fbe40e4e87a260db148873fb5231475e0afbfec2
simple <shwncho4@gmail.com>
2022-09-19 15:45:56 +0900

    feat: main

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,14 +4,17 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import "normalize.css";
 
-import "bootstrap/dist/css/bootstrap-utilities.css"
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+import "bootstrap/dist/css/bootstrap-utilities.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(ElementPlus)
+app.use(ElementPlus);
+
 app.mount("#app");
--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,43 +1,40 @@
 <script setup lang="ts">
-  import {ref} from "vue";
-  import axios from 'axios';
-  import {useRouter} from "vue-router";
+import { ref } from "vue";
 
+import axios from "axios";
+import { useRouter } from "vue-router";
 
-  const title =ref("")
-  const content=ref("")
-  const router = useRouter();
+const title = ref("");
+const content = ref("");
 
-  const write=function (){
-    axios.post("http://localhost:8080/posts",{
-      title: title.value,
-      content: content.value
-    })
-    .then(()=>{
-      /**
-       * push 대신 replace 사용
-       * => 글 작성하고 홈 화면에서 뒤로가기 눌렀을 때 다시 글 작성화면으로 라우팅 되는거 방지할 수 있다.
-       */
-      router.replace({name: "home"});
-    });
-  }
+const router = useRouter();
+
+const write = function () {
+  axios
+      .post("http://localhost:8080/posts", {
+        title: title.value,
+        content: content.value,
+      })
+      .then(() => {
+        router.replace({ name: "home" });
+      });
+};
 </script>
 
 <template>
   <div>
-    <input v-model="title" placeholder="제목을 입력해주세요" />
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
   </div>
 
   <div class="mt-2">
-    <el-input v-model="content" type="textarea" rows="15"/>
+    <el-input v-model="content" type="textarea" rows="15" />
   </div>
 
   <div class="mt-2">
-    <el-button type="primary" @click="write()">등록</el-button>
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
+    </div>
   </div>
-
 </template>
 
-<style>
-
-</style>
+<style></style>
